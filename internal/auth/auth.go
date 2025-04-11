4341fb9e7b3d145f25de9f201b484301a8869795
tylerbartlett24 <crazyquilt86@gmail.com>
2025-04-11 11:03:43 -0230

    Added working tests.

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,11 +12,11 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "", nil//rrNoAuthHeaderIncluded
+		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", nil//errors.New("malformed authorization header")
+		return "", errors.New("malformed authorization header")
 	}
 
 	return splitAuth[1], nil
