baf6429a0acd8e138df9bf2caa37ab0ab91528c2
tylerbartlett24 <crazyquilt86@gmail.com>
2025-04-11 12:01:03 -0230

    Fixed formatting.

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -9,7 +9,7 @@ import (
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
-func GetAPIKey(headers http.Header)(string, error){
+func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
@@ -19,4 +19,5 @@ func GetAPIKey(headers http.Header)(string, error){
 		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[1], nil}
+	return splitAuth[1], nil
+}
